94b50961fc758a84cb5760cca983efa608681694
fernanda caramico 123 <fernandacaramico@gmail.com>
2022-05-24 10:29:36 -0300

    first commit

---
+++ script-criacao-tabelas-aquario-e-medida.sql
@@ -0,0 +1,50 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+/* para workbench - local - desenvolvimento */
+-- CREATE DATABASE acquatec;
+
+-- USE acquatec;
+
+
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	descricao VARCHAR(300)
+);
+
+/* altere esta tabela de acordo com o que está em INSERT de sua API do arduino */
+
+create table medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT,
+	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+);
+
+
+/* para sql server - remoto - produção */
+
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY IDENTITY(1,1),
+	descricao VARCHAR(300)
+);
+
+/* altere esta tabela de acordo com o que está em INSERT de sua API do arduino */
+
+CREATE TABLE medida (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
+);
